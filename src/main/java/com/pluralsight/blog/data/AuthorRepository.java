0e5b2dd98d7b854994e96fc1601644e56e9daa14
Justin K <jstkim09@gmail.com>
2022-02-19 18:08:16 -0500

    Update AuthorRepository.java

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 
 @RepositoryRestResource(exported = false)
-@Component
+//@Component
 public interface AuthorRepository extends JpaRepository<Author, Long> {}
 
 /*
